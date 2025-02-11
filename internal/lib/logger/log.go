b9ebbd5112e76462928c7ad35b5c3bb7a1ae06ee
zaqbez39me <korolov_k@mail.ru>
2025-02-11 04:52:51 +0300

    feat: add custom logger and initializer for it

---
+++ internal/lib/logger/log.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+// Setup initialize the log instance
+func Setup(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	slog.SetDefault(logger)
+	return logger
+}
